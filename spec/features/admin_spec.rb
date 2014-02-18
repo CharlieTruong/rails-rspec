39bd4add1a82001211d6321d556cbaa978c3ae9a
Apprentice <apprentice@dbc-chicago-004.local>
2014-02-18 16:37:07 -0600

    working on completing pending rspec tests in admin_spec file

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -2,11 +2,31 @@ require 'spec_helper'
 
 feature 'Admin panel' do
   context "on admin homepage" do
-    it "can see a list of recent posts"
+    it "can see a list of recent posts" do
+      page.driver.browser.authorize 'geek', 'jock'
 
-    it "can edit a post by clicking the edit link next to a post"
+      Post.create!(title: "Title", content: "Content")
+      visit admin_posts_path
+      page.should have_content("Title")
+    end
 
-    it "can delete a post by clicking the delete link next to a post"
+    it "can edit a post by clicking the edit link next to a post" do
+      page.driver.browser.authorize 'geek', 'jock'
+
+      Post.create!(title: "Title", content: "Content")
+      visit admin_posts_path
+      click_link "Edit"
+      expect(page).to have_content("Edit Title")
+    end
+
+    it "can delete a post by clicking the delete link next to a post" do
+      page.driver.browser.authorize 'geek', 'jock'
+
+      Post.create!(title: "Title", content: "Content")
+      visit admin_posts_path
+      click_link "Delete"
+      expect(page).to have_no_content("Title")
+    end
 
     it "can create a new post and view it" do
        visit new_admin_post_url
@@ -25,14 +45,21 @@ feature 'Admin panel' do
 
   context "editing post" do
     it "can mark an existing post as unpublished" do
-      pending # remove this line when you're working on implementing this test
+      page.driver.browser.authorize 'geek', 'jock'
 
+      Post.create!(title: "Title", content: "Content", is_published: true)
+      visit admin_posts_path
+      click_link "Edit"
+      uncheck('post_is_published')
+      click_button "Save"
       page.should have_content "Published: false"
     end
   end
 
   context "on post show page" do
-    it "can visit a post show page by clicking the title"
+    it "can visit a post show page by clicking the title" do
+
+    end
 
     it "can see an edit link that takes you to the edit post path"
 
